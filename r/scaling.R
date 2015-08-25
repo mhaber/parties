cf52336ec632ecbcbaed44996b3cf3427ba723a1
Matthias Haber <haber@hertie-school.org>
2015-08-25 19:16:45 +0200

    wordfish with all congresses

--- r/scaling.R
+++ r/scaling.R
@@ -10,9 +10,154 @@ library(stringr)
 ######################
 # Loading Data Files #
 ######################
+dir="data/sisterParties/cduCongresses"
+
+### Load txt files into corpus
+corpus=Corpus(DirSource(dir), readerControl=list(reader=readPlain, language="de", load="F")) #Adjust Languages
+
+### Removal Word List
+wordlist <- read.csv("data/stopWords.txt", stringsAsFactors=F) # wordlist.txt is list of words that you want to exclude. 
+wordlist=wordlist[,1]
+
+######################
+# Text Preprocessing #
+######################
+corpusClean <- tm_map(corpus, gsub, pattern = "\\(.*)", replacement = "") #remove text in brackets
+corpusClean <- tm_map(corpusClean, stripWhitespace) # remove extra white spaces
+corpusClean <- tm_map(corpusClean, tolower) # convert words to lower case
+corpusClean <- tm_map(corpusClean, removePunctuation) # remove punctuation
+corpusClean <- tm_map(corpusClean, removeWords, wordlist) #remove unwanted words
+#corpusClean <- tm_map(corpusClean, removeWords, c("")) # removes indiviual words if not using wordlist.txt
+corpusClean <- tm_map(corpusClean, removeNumbers) # remove numbers
+corpusClean <- tm_map(corpusClean, removeWords, stopwords("german")) # remove stopwords, Adjust Languages
+corpusClean <- tm_map(corpusClean,stemDocument, language="german") # stem words, Adjust Languages
+corpusClean <- tm_map(corpusClean, PlainTextDocument) #convert everything to plain text
+names(corpusClean) <- names(corpus)
+
+# Create object to remove frequent and sparse terms
+ndocs <- length(corpusClean)
+minDocFreq <- ndocs * 0.01 # keep words that occur at least in % of documents
+maxDocFreq <- ndocs * 0.99 # keep words that occur at the most in % of documents
+
+### Creating a DocumentTermMatrix
+#dtm <- DocumentTermMatrix(corpusClean) # normal dtm without word bounds
+dtm<- DocumentTermMatrix(corpusClean, control = list(bounds = list(global = c(minDocFreq, maxDocFreq))))
+
+# inspect(dtm[1:5,100:105]) # lets you inspect the first 5 documents and the 100-105 words
+# findFreqTerms(dtm, 50) # list the most frequent words occuring at least 5 times
+# dtm<- removeSparseTerms(dtm, 0.4) # remove sparse terms (at least 40%) to reduce size
+
+### Creating a Word-Frequency Matrix
+wfm <- wfm(dtm, word.margin = 2)
+
+#######################
+# Wordfish Estimation #
+#######################
+
+### Run Wordfish
+wf.res <- wordfish(wfm, dir=c(1582, 1609)) #dir=c() is used to anchor documents; i.e. one text has a lower value than another
+
+###############
+# Diagnostics #
+###############
+
+## create data frame
+
+# party leader vector
+leader <- rep(0,1659) 
+leader[c(24,54,84,93,193,149,300,338,389,493,557,599,674,713,761,820,854,903,923,999,
+         948,1013,1058,1060,1068,1106,1110,1167,1175, 1230,1242,1290,1293,1304,1317,
+         1371,1383,1433,1479,1498,1526,1583,1610)] <- 1
+
+# party vector
+party <- rep("CDU",1659)
+party[c(54,93,149,338,557,713,820,923,948,1013,1068,1110,1175,1242,1293,1317,
+        1383,1479,1526,1610)] <- "CSU"
+
+# party congress vector
+congress <- gsub("_.*","",wf.res$docs)
+congress <- gsub(" parteitag ","_",congress)
+
+# combined data frame
+thetas <- data.frame(substr(wf.res$docs, 1, 4),
+                     congress,
+                     wf.res$docs,
+                     party, 
+                     leader,
+                     wf.res$theta,
+                     wf.res$theta - (1.96*wf.res$se.theta),
+                     wf.res$theta + (1.96*wf.res$se.theta))
+names(thetas)[1] <- "year"
+names(thetas)[2] <- "congress"
+names(thetas)[3] <- "speaker"
+names(thetas)[4] <- "party"
+names(thetas)[5] <- "leader"
+names(thetas)[6] <- "mean"
+names(thetas)[7] <- "lower"
+names(thetas)[8] <- "upper"
+rownames(thetas) <- NULL
+
+# clean speaker names
+thetas$speaker <- gsub("^.*_(.*).txt.*$","\\1",thetas$speaker) 
+thetas$speaker <- gsub("^.*_(.*).txt.*$","\\1",thetas$speaker)
+
+# mean party position per congress with uncertainties
+partyMean <- rep(aggregate(thetas$mean, list(thetas$congress), FUN="mean")[,2],
+                 table(thetas$congress))
+partyMeanLower <- rep(aggregate(thetas$lower, list(thetas$congress), FUN="mean")[,2],
+                      table(thetas$congress))
+partyMeanUpper <- rep(aggregate(thetas$upper, list(thetas$congress), FUN="mean")[,2],
+                      table(thetas$congress))
+thetas$partyMean <- partyMean
+thetas$partyMeanLower <- partyMeanLower
+thetas$partyMeanUpper <- partyMeanUpper
+
+### summary of results
+#summary(wf.res)
+
+###  Mean, Median and Variance
+#mean <- mean(wf.res$theta)
+#median <- median(wf.res$theta)
+#variance <- var(wf.res$theta)
+
+###  Plot results
+## wordfish plot sorted by estimate
+# plot(wf.res)
+
+## ggplot with nice customization
+p1 <- ggplot(subset(thetas,leader==1), aes(x=congress, y=mean, group=1)) +
+  geom_errorbar(width=.1, aes(ymin=lower, ymax=upper)) +
+  geom_point(shape=20, size=4) +
+  #geom_point(aes(y=partyMean, shape=20, size=4))  +
+  geom_text(aes(label=party, color=party),hjust=.5, vjust=-.5) +
+  guides(colour=FALSE) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
+  geom_hline(yintercept = mean(thetas$mean), linetype = "dashed") +
+  coord_flip() + theme_bw() + xlab("") + ylab("") +
+  ggtitle("Positions of CDU and CSU Leaders 1990-2011")
+ggsave(p1, file="figures/Positions of CDU and CSU 1990-2011.pdf", width=12, height=10)
+
+p2 <- ggplot(subset(thetas,leader==1), aes(x=congress, y=mean, group=1)) +
+  geom_errorbar(width=.1, aes(ymin=lower, ymax=upper)) +
+  geom_point(shape=20, size=4) + 
+  geom_text(aes(label=speaker, color=party),hjust=.5, vjust=-.5) +
+  guides(colour=FALSE) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
+  geom_hline(yintercept = mean(thetas$mean), linetype = "dashed") +
+  coord_flip() + theme_bw() + xlab("") + ylab("") +
+  ggtitle("Positions of CDU and CSU Leaders 1990-2011")
+ggsave(p2, file="figures/Positions of CDU and CSU Leaders 1990-2011.pdf", width=12, height=10)
+
+##################################################################################
+############################### Only Party Leaders ###############################
+##################################################################################
+
+######################
+# Loading Data Files #
+######################
 
 ### Set Location of Files
-dir="data/sisterParties"
+dir="data/sisterParties/leaders"
 
 ### Load txt files into corpus
 corpus=Corpus(DirSource(dir), readerControl=list(reader=readPlain, language="de", load="F")) #Adjust Languages
