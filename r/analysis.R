5380f3c2d0bbdf56718f5bcb90d84d4efce0d64e
Matthias Haber <haber@hertie-school.org>
2015-11-26 18:01:13 +0100

    added ches data

--- r/analysis.R
+++ r/analysis.R
@@ -4,16 +4,17 @@ library(stringr)
 library(RSQLite)
 library(manifestoR)
 library(foreign)
+library(readstata13)
 library(dplyr)
 library(countrycode)
-
+library(lubridate)
 #####################
 ### Manifesto Project
 
 mp_setapikey(key = "03bc3947326cfae8288519d7351411a8") # get from manifesto profile page
 manifesto <- mp_maindataset()
-parties <- manifesto %>% dplyr::mutate(iso3 = countrycode(countryname, "country.name", "iso3n")) %>% 
-  select(iso3, date, party1=party, partyname, parfam)
+manifestoSample <- manifesto %>% dplyr::mutate(iso3 = countrycode(countryname, "country.name", "iso3n")) %>% 
+  select(iso3, date, party, partyname, parfam, rile)
 
 ###################
 ### Golder PEC Data
@@ -21,29 +22,63 @@ parties <- manifesto %>% dplyr::mutate(iso3 = countrycode(countryname, "country.
 golder <- read.dta("data/golder.dta")
 golder$iso3 <- countrycode::countrycode(golder$countryname, "country.name", "iso3n")
 
-golder$partyname1 <- parties$partyname[match(golder$party1, parties$party1)]
-
-
-
-pec <- dplyr::full_join(golder,parties, by="party1")
-
+#############################
+### Chapel Hill Expert Survey 
+ches <- readstata13::read.dta13("data/ches.dta")
+chesSample <- ches %>%  dplyr::select(year, cmp_id, lrgen) 
 
 ############
 ### Parl Gov 
-
 # connect to the sqlite file
 con <- dbConnect(RSQLite::SQLite(), "data/parlgov-stable.db")
 
 # get a list of all tables
 alltables <- dbListTables(con)
 
-# get the cabinets table as a data.frame
-cabinets <- dbGetQuery(con,'select * from view_cabinet')
+# get the parlGov table as a data.frame
+parlGov <- dbGetQuery(con,'select * from view_party')
 
-# get data for Germany
-cabinetsGer <- subset(cabinets, cabinets$country_name=="Germany") # cabinets
-cdu <- subset (cabinetsGer, cabinetsGer$party_name_short=="CDU")
-csu <- subset (cabinetsGer, cabinetsGer$party_name_short=="CSU")
+################
+# Merge all Data
+parties <- golder
+
+# Add year
+parties$year <- lubridate::year(lubridate::ymd(parties$election+19000000))
+
+# add party names
+parties$party1Name <- manifestoSample$partyname[match(golder$party1, manifestoSample$party)]
+parties$party1Fam <- manifestoSample$parfam[match(golder$party1, manifestoSample$party)]
+
+parties$party2Name <- manifestoSample$partyname[match(golder$party2, manifestoSample$party)]
+parties$party2Fam <- manifestoSample$parfam[match(golder$party2, manifestoSample$party)]
+
+# add positions
+parties %>% 
+  mutate(party1Ches=chesSample$lrgen[match(golder$party1, chesSample$cmp_id)])
+
+###########################
+### Reconciliation Function
+reconcile <- function(query, typeId) {
+  dataUrl <- paste0("https://tools.wmflabs.org/wikidata-reconcile/?query={%22query%22%3A%22", 
+                    URLencode(query), "%22%2C%22type%22%3A%22", typeId, "%22}")
+  
+  jsonlite::fromJSON(dataUrl)$result[1, "name"]
+}
+
+names <- parlGov %>% dplyr::group_by(country_name) %>% filter(election_id==731) %>% ungroup() %>% select(party_name_english)
+sapply(names, reconcile, typeId = "Q7278")
 
 
-#TESTING 
+
+
+
+# match party names with golder
+golder %>%  group_by(country) %>% 
+  dplyr::rowwise() %>% 
+  dplyr::mutate(partypos1 = parlGov$left_right[agrepl(parlGov$party_name_english,golder$partyname1, max.distance = .1)])
+
+
+golder$partypos1 <- parlGov$left_right[agrep(parlGov$party_name_english, golder$partyname1, max.distance=0.1)]
+golder$partypos1 <- sapply(parlGov$party_name_english, agrep, x=golder$partyname1, max.distance=0.1)
+###########
+### Testing
