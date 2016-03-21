5d21f680e71c5e96bcaaaa6330d9bccbfc0c09ce
Matthias Haber <haber@hertie-school.org>
2016-03-21 16:16:36 +0100

    added scatterplot instead of table 3

--- r/analysis.R
+++ r/analysis.R
@@ -162,3 +162,58 @@ summary(model12)
 summary(model13)
 summary(model14)
 summary(model15)
+
+#Scatterplot
+## Custom Theme
+theme_Publication <- function(base_size=14, base_family="sans") {
+  library(grid)
+  library(ggthemes)
+  (theme_foundation(base_size=base_size, base_family=base_family)
+  + theme(plot.title = element_text(face = "bold",
+                                    size = rel(1.2), hjust = 0.5),
+          text = element_text(),
+          panel.background = element_rect(colour = NA),
+          plot.background = element_rect(colour = NA),
+          panel.border = element_rect(colour = NA),
+          axis.title = element_text(face = "bold",size = rel(1)),
+          axis.title.y = element_text(angle=90,vjust =2),
+          axis.title.x = element_text(vjust = -0.2),
+          axis.text = element_text(hjust = 1),
+          axis.line = element_line(colour="black"),
+          axis.ticks = element_line(),
+          panel.grid.major = element_line(colour="#f0f0f0"),
+          panel.grid.minor = element_blank(),
+          legend.key = element_rect(colour = NA),
+          legend.position = "bottom",
+          legend.direction = "horizontal",
+          legend.key.size= unit(0.2, "cm"),
+          legend.margin = unit(0, "cm"),
+          legend.title = element_text(face="italic"),
+          plot.margin=unit(c(10,5,5,5),"mm"),
+          strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
+          strip.text = element_text(face="bold")
+  ))
+  
+}
+
+scale_fill_Publication <- function(...){
+  library(scales)
+  discrete_scale("fill","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+  
+}
+
+scale_colour_Publication <- function(...){
+  library(scales)
+  discrete_scale("colour","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+  
+}
+
+regressionPlot <- ggplot(distCduCsu, aes(x=lag(voteCdu), y=distLeader)) +
+  geom_point() +
+  geom_smooth(method=lm, formula = y ~ lag(x), col = "black") +
+  xlab("(Lagged) Percentage of respondents who would vote for the CDU") +
+  ylab("Distance between CDU and CSU Leaders") +
+  ggtitle("Effect of Previous Expected Vote Share on the Distance between Party Leader Posiitions\n (Correlation -0.5)")+
+  theme_Publication()
+save(regressionPlot, file="figures/regressionPlot.RData")
+
